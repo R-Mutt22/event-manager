0ef1bd52f76ed8849e7c6308e91d9c33d69f235d
R-Mutt22 <118209167+R-Mutt22@users.noreply.github.com>
2024-07-10 19:38:47 -0300

    Add files via upload

--- programs/event-manager/src/lib.rs
+++ programs/event-manager/src/lib.rs
@@ -1,4 +1,8 @@
-use anchor_lang::prelude::*;
+use { anchor_lang::prelude::*, crate::instructions::*} ;
+
+mod collections;
+mod instructions;
+mod utils;
 
 declare_id!("3LYBRHRpr959ApFB5FUxNdScgyMkpQXkvph4dCeKqvox");
 
@@ -6,10 +10,49 @@ declare_id!("3LYBRHRpr959ApFB5FUxNdScgyMkpQXkvph4dCeKqvox");
 pub mod event_manager {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn create_event(
+        ctx: Context<CreateEvent>,
+        name: String,
+        ticket_price: u64,
+    ) -> Result<()> {
+        instructions::create_event::handle(ctx, name, ticket_price)
+    }
+
+     // sponsor event (get event mint tokens)
+     pub fn sponsor_event (
+        ctx: Context<Sponsor>,
+        quantity: u64,
+    ) -> Result<()> {
+        instructions::sponsor::handle(ctx, quantity)
+    }
+
+    // buy tickets
+    pub fn buy_tickets (
+        ctx: Context<BuyTickets>,
+        quantity: u64,
+    ) -> Result<()> {
+        instructions::buy_tickets::handle(ctx, quantity)
+    }
+
+     // withdraw funds
+     pub fn withdraw_funds(
+        ctx: Context<WithdrawFunds>,
+        amount: u64,
+    ) -> Result<()> {
+        instructions::withdraw_funds::handle(ctx, amount)
+    }
+
+    // close event
+    pub fn close_event (
+        ctx: Context<CloseEvent>
+    ) -> Result<()> {
+        instructions::close_event::handle(ctx)
+    }
+
+    // withdraw earnings
+    pub fn withdraw_earnings(
+        ctx: Context<WithdrawEarnings>
+    ) -> Result<()> {
+        instructions::withdraw_earnings::handle(ctx)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
